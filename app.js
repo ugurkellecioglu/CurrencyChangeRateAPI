998e3a76b1b5508a6698a0b28e9ac2bd0f287d33
ugurkellecioglu <ugur.kellecioglu@outlook.com>
2021-08-27 13:21:15 +0300

    from to amount end points

--- app.js
+++ app.js
@@ -42,9 +42,9 @@ app.get('/:from/', async function (req, res) {
     }
 })
 
-app.get('/:from/:amount', async function (req, res) {
+app.get('/:from/', async function (req, res) {
     try {
-        request(`https://www.x-rates.com/table/?from=${req.params.from}&amount=${req.params.amount}`, function (error, response, html) {
+        request(`https://www.x-rates.com/table/?from=${req.params.from}&amount=${req.query.amount}`, function (error, response, html) {
             if (!error && response.statusCode == 200) {
                 const resp = [[{ from: req.params.from }]]
                 const currencyrates = []
@@ -77,6 +77,8 @@ app.get('/:from/:amount', async function (req, res) {
         res.json({ error: 'Something went wrong', status: 400 })
     }
 })
+
+
 
 app.get('/:from/:to', async function (req, res) {
     if (req.params.from === req.params.to) res.json({ from: req.params.from, to: req.params.to, currency: 1 })
@@ -129,5 +131,58 @@ app.get('/:from/:to', async function (req, res) {
         }
     });
 })
+
+app.get('/:from/:to/', async function (req, res) {
+    if (req.params.from === req.params.to) res.json({ from: req.params.from, to: req.params.to, currency: 1 })
+    request(`https://www.x-rates.com/table/?from=${req.params.from}&amount=${req.query.amount}`, function (error, response, html) {
+        if (!error && response.statusCode == 200) {
+            try {
+                const result = []
+                const dom = new JSDOM(html);
+                const document = dom.window.document;
+                const list = document.querySelector('.tablesorter.ratesTable > tbody').children
+                var arr = Array.prototype.slice.call(list)
+
+                const temp = arr.filter((item) => {
+                    const td = item.children[1];
+                    const a = td.children[0];
+                    const href = a.getAttribute('href')
+                    const symbole = href.substring(href.indexOf('to') + 3)
+                    return symbole.toLowerCase() === req.params.to
+                })
+                if (temp.length === 0) {
+                    res.json(
+                        {
+                            error: `There is no match such currency symbol: ${req.params.to}`,
+                            status: 404
+                        }
+                    )
+                    return ''
+                }
+                result.push({
+                    from: req.params.from,
+                    to: req.params.to,
+                    rate: temp[0].children[1].textContent
+                })
+                res.json(result)
+            } catch (error) {
+                res.json(
+                    {
+                        error: `There is no such currency symbole: ${req.params.from}`,
+                        status: 404
+                    }
+                )
+            }
+        } else if (error) {
+            res.json(
+                {
+                    error: `Something went wrong`,
+                    status: 400
+                }
+            )
+        }
+    });
+})
+
 const PORT = process.env.PORT || 8008
 app.listen(PORT, () => console.log(`server is up at http://localhost:${PORT}/`))
